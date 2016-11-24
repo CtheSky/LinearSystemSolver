e3ab63c42c901ec63e3fb2e8036729c99b4138a0
CtheSky <cthesky@yeah.net>
2016-11-24 21:28:32 +0800

    feat: basic vector representation

---
+++ vector.py
@@ -0,0 +1,19 @@
+class Vector:
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
