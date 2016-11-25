7e115b9d6bacdc70e4511a56dff42c2828fe80de
CtheSky <cthesky@yeah.net>
2016-11-25 10:04:47 +0800

    feat: plus, minus, scalar multiplication

--- vector.py
+++ vector.py
@@ -12,6 +12,18 @@ class Vector:
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
+    def plus(self, v):
+        new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def times_scalar(self, c):
+        new_coordinates = [c * x for x in self.coordinates]
+        return Vector(new_coordinates)
+
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
