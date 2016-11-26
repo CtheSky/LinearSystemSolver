f1048d107e7e3681dfb9f5e866500b13a9daef6a
CtheSky <cthesky@yeah.net>
2016-11-26 12:25:26 +0800

    feat: dot product & compute angle between 2 vectors

--- vector.py
+++ vector.py
@@ -1,6 +1,10 @@
-from math import sqrt
+from math import sqrt, acos, pi
+
 
 class Vector:
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    CANNOT_COMPUTE_ANGLE_WITH_ZERO_VECTOR_MSG = 'Cannot compute angle with zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -36,7 +40,28 @@ class Vector:
             return self.times_scalar(1. / magnitude)
 
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def dot(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
+
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if in_degrees:
+                degrees_per_radian = 180. / pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.CANNOT_COMPUTE_ANGLE_WITH_ZERO_VECTOR_MSG)
+            else:
+                raise e
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
