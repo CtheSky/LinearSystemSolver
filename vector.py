641b239f815366ecd19c7edf65dd01921a4a0cf4
CtheSky <cthesky@yeah.net>
2016-11-26 13:39:17 +0800

    feat: use decimal ro represent coordinates

--- vector.py
+++ vector.py
@@ -1,4 +1,7 @@
 from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 
 class Vector:
@@ -9,7 +12,7 @@ class Vector:
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -17,6 +20,22 @@ class Vector:
 
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
+
+    def magnitude(self):
+        coordinates_squared = [x ** 2 for x in self.coordinates]
+        return Decimal(sqrt(sum(coordinates_squared)))
+
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(Decimal('1.0') / magnitude)
+
+        except ZeroDivisionError:
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def times_scalar(self, c):
+        new_coordinates = [Decimal(c) * x for x in self.coordinates]
+        return Vector(new_coordinates)
 
     def plus(self, v):
         new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
@@ -26,22 +45,6 @@ class Vector:
         new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
         return Vector(new_coordinates)
 
-    def times_scalar(self, c):
-        new_coordinates = [c * x for x in self.coordinates]
-        return Vector(new_coordinates)
-
-    def magnitude(self):
-        coordinates_squared = [x ** 2 for x in self.coordinates]
-        return sqrt(sum(coordinates_squared))
-
-    def normalized(self):
-        try:
-            magnitude = self.magnitude()
-            return self.times_scalar(1. / magnitude)
-
-        except ZeroDivisionError:
-            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
-
     def dot(self, v):
         return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
 
@@ -49,10 +52,10 @@ class Vector:
         try:
             u1 = self.normalized()
             u2 = v.normalized()
-            angle_in_radians = acos(u1.dot(u2))
+            angle_in_radians = Decimal(acos(u1.dot(u2)))
 
             if in_degrees:
-                degrees_per_radian = 180. / pi
+                degrees_per_radian = Decimal('180.0') / Decimal(pi)
                 return angle_in_radians * degrees_per_radian
             else:
                 return angle_in_radians
