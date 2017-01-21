6954dce8ac1bfdcf3ab68f3f514d6026892f1be0
CtheSky <cthesky@yeah.net>
2017-01-21 19:26:28 +0800

    test: compute solution

--- test/linear_system_test.py
+++ test/linear_system_test.py
@@ -131,3 +131,22 @@ class LinearSystemTest(unittest.TestCase):
                                       constant_term=Decimal('7') / Decimal('9')) and
                         r[2] == Plane(normal_vector=Vector(['0', '0', '1']),
                                       constant_term=Decimal('2') / Decimal('9')))
+
+    def test_compute_solution(self):
+        p1 = Plane(normal_vector=Vector(['5.862', '1.178', '-10.366']), constant_term='-8.15')
+        p2 = Plane(normal_vector=Vector(['-2.931', '-0.589', '5.183']), constant_term='-4.075')
+        s = LinearSystem([p1, p2])
+        self.assertEqual(s.compute_solution(), LinearSystem.NO_SOLUTIONS_MSG)
+
+        p1 = Plane(normal_vector=Vector(['8.631', '5.112', '-1.816']), constant_term='-5.113')
+        p2 = Plane(normal_vector=Vector(['4.315', '11.132', '-5.27']), constant_term='-6.775')
+        p3 = Plane(normal_vector=Vector(['-2.158', '3.01', '-1.727']), constant_term='-0.831')
+        s = LinearSystem([p1, p2, p3])
+        self.assertEqual(s.compute_solution(), LinearSystem.INF_SOLUTIONS_MSG)
+
+        p1 = Plane(normal_vector=Vector(['5.262', '2.739', '-9.878']), constant_term='-3.441')
+        p2 = Plane(normal_vector=Vector(['5.111', '6.358', '7.638']), constant_term='-2.152')
+        p3 = Plane(normal_vector=Vector(['2.016', '-9.924', '-1.367']), constant_term='-9.278')
+        p4 = Plane(normal_vector=Vector(['2.167', '-13.543', '-18.883']), constant_term='-10.567')
+        s = LinearSystem([p1, p2, p3, p4])
+        self.assertEqual(s.compute_solution(), Vector(['-1.177', '0.707', '-0.083']))
